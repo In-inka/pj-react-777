b6ca0d7975566332cf804adeab0b8b54eae12020
IvashkaVIP <ivashka.co@gmail.com>
2023-10-21 22:36:36 +0300

    add css

--- src/components/DayProducts/DayProducts.jsx
+++ src/components/DayProducts/DayProducts.jsx
@@ -1,10 +1,96 @@
-import { Wrapper } from "./DayProducts.styled"
+import { Wrapper, Title, SubTitle, AddProduct, ItemWrapper, ItemName, ItemField, WrapMobile, IconWrapper, StyledLink } from "./DayProducts.styled"
+import sprite from "../../assets/sprite.svg"
 
 export const DayProducts = ({children}) => {
     
-    console.log(children);
+    console.log(products);
     
-    return (<Wrapper>
-        {children}
-    </Wrapper>)
+    return (
+      <Wrapper>
+        <Title>
+          <SubTitle>Products</SubTitle>
+          <StyledLink to="/first">
+            <AddProduct>Add Product</AddProduct>
+            <IconWrapper color="#E6533C">
+              <use href={`${sprite}#icon-logo`} />
+            </IconWrapper>
+          </StyledLink>
+        </Title>
+        <ProductsTable>{children}</ProductsTable>
+      </Wrapper>
+    );
 }
+
+export const ProductsTable = ({ children }) => {
+    
+    return (
+      <>
+        <ItemProduct>Title</ItemProduct>
+        <ItemProduct>Category</ItemProduct>
+        <WrapMobile>
+          <ItemProduct>Calories</ItemProduct>
+          <ItemProduct>Weight</ItemProduct>
+          <ItemProduct>Recommend</ItemProduct>
+        </WrapMobile>
+      </>
+    );
+}
+
+export const ItemProduct = ({ children }) => {
+    // console.log(children);
+    return (
+        <ItemWrapper>
+        <ItemName>{children}</ItemName>
+        <ItemField width="auto" height="38px">{children}</ItemField>
+        </ItemWrapper>
+    );
+}
+
+
+let products = [
+  {
+    _id: {
+      $oid: '5d51694902b2373622ff5773',
+    },
+    weight: 100,
+    calories: 340,
+    category: 'dairy',
+    title: 'Danbo cheese',
+    groupBloodNotAllowed: {
+      1: true,
+      2: true,
+      3: false,
+      4: false,
+    },
+  },
+  {
+    _id: {
+      $oid: '5d51694902b2373622ff5b7f',
+    },
+    weight: 100,
+    calories: 112,
+    category: 'fish',
+    title: 'marlin',
+    groupBloodNotAllowed: {
+      1: false,
+      2: false,
+      3: false,
+      4: false,
+    },
+  },
+  {
+    _id: {
+      $oid: '5d51694902b2373622ff5e13',
+    },
+    weight: 100,
+    calories: 17,
+    category: 'vegetables and herbs',
+    title: 'Salads Belaya Dacha Delicate root',
+    groupBloodNotAllowed: {
+      1: false,
+      2: false,
+      3: false,
+      4: false,
+    },
+  },
+];
--- src/components/DayProducts/DayProducts.styled.jsx
+++ src/components/DayProducts/DayProducts.styled.jsx
@@ -1,18 +1,121 @@
 import styled from 'styled-components';
+import { Link } from 'react-router-dom';
 
 export const Wrapper = styled.div`
   /* 320-375 */
-  width: 335px;
-  height: 824px;
-
+  padding: 16px;
   border: 1px solid rgb(239, 237, 235, 0.2);
   border-radius: 12px;
-
   background-color: rgb(239, 237, 235, 0.05);
 
-  @media (max-width: 767px) {
+  @media screen and (min-width: 375px) {
+    width: 375px
   }
 
-  @media (min-width: 768px) and (max-width: 1440px) {
+  @media screen and (min-width: 768px) {
+  }
+  @media screen and (min-width: 1440px) {
   }
 `;
+
+export const Title = styled.div`
+  /* 320-375 */
+  display: flex;
+  justify-content: space-between;
+  margin-bottom: 22px;
+
+  @media screen and (min-width: 768px) {
+  }
+  @media screen and (min-width: 1440px) {
+  }
+`;
+
+export const SubTitle = styled.div`
+  /* 320-375 */
+  font-size: 18px;
+  color: rgb(239, 237, 232, 0.5);
+
+  @media screen and (min-width: 768px) {
+  }
+  @media screen and (min-width: 1440px) {
+  }
+`;
+
+export const AddProduct = styled.p`
+  /* 320-375 */
+  font-size: 18px;
+  color: rgb(230, 83, 60);
+
+  @media screen and (min-width: 768px) {
+  }
+  @media screen and (min-width: 1440px) {
+  }
+`;
+
+export const WrapMobile = styled.div`
+  display: flex;
+  gap: 16px;
+
+  @media screen and (min-width: 768px) {
+  }
+  @media screen and (min-width: 1440px) {
+  }
+`;
+
+export const ItemWrapper = styled.div`
+  /* 320-375 */
+  font-size: 18px;
+  color: rgb(230, 83, 60);
+
+  @media screen and (min-width: 768px) {
+  }
+  @media screen and (min-width: 1440px) {
+  }
+`;
+
+export const ItemName = styled.p`
+  /* 320-375 */
+  font-size: 18px;
+  color: rgb(239, 137, 100);
+  margin-bottom: 8px;
+
+  @media screen and (min-width: 768px) {
+  }
+  @media screen and (min-width: 1440px) {
+  }
+`;
+
+export const ItemField = styled.div`
+  /* 320-375 */
+  display: flex;
+  align-items: center;
+  margin-bottom: 16px;
+  padding: 10px 14px;
+  font-size: 18px;
+  color: rgb(239, 237, 232);
+  width: ${(prop) => prop.width};
+  height: ${(prop) => prop.height};
+  border: 1px solid rgb(239, 237, 235, 0.2);
+  border-radius: 12px;
+  background-color: rgb(239, 237, 235, 0.05);
+
+  @media screen and (min-width: 768px) {
+  }
+  @media screen and (min-width: 1440px) {
+  }
+`;
+
+export const IconWrapper = styled.svg`
+  width: ${(props) => props.size || '12px'};
+  height: ${(props) => props.size || '12px'};
+  fill: ${(props) => props.color || '#ffffff'};
+  display: inline-block;
+  margin-left: 9px;
+ `;
+
+ export const StyledLink = styled(Link)`
+   display: flex;
+   align-items: baseline;
+   /* padding: 8px 16px; */
+   color: rgb(230, 83, 60);
+ `;
