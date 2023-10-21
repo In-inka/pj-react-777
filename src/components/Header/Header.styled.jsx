22f87c9f69cf7c3d99f6af365ff1de4d2de826cb
Inna Shkilna <shkinai.1995@gmail.com>
2023-10-21 22:05:52 +0300

    add @font-face

--- src/components/Header/Header.styled.jsx
+++ src/components/Header/Header.styled.jsx
@@ -4,6 +4,7 @@ import { ContainerStyled } from '../../Styled/SharedStyled/ContainerStyled';
 export const Container = styled.nav`
   ${ContainerStyled}
   margin: 25px;
+  font-weight: 700;
 
   display: flex;
   gap: 15px;
--- src/main.jsx
+++ src/main.jsx
@@ -2,11 +2,13 @@ import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
+import { FontStyles } from './FontStyle.jsx';
 //import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter basename="/pj-react-777">
+      <FontStyles />
       <App />
     </BrowserRouter>
   </React.StrictMode>,
