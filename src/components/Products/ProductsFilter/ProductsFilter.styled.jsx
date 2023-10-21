d0861e379b9cd94e7c65c299507493cfeade92db
Ihor Poliakov <piv2003@gmail.com>
2023-10-21 23:55:20 +0200

    Update ProductsFilter.styled.jsx

--- src/components/Products/ProductsFilter/ProductsFilter.styled.jsx
+++ src/components/Products/ProductsFilter/ProductsFilter.styled.jsx
@@ -3,3 +3,46 @@ import styled from 'styled-components';
 export const ProductsFilterLabel = styled.label`
   position: relative;
 `;
+
+export const ProductsFilterSearch = styled.input`
+  width: 320px;
+  background-color: transparent;
+  color: #efede8;
+  border-radius: 12px;
+  border: 1px solid rgba(239, 237, 232, 0.3);
+  font-size: 14px;
+  @media screen and (min-width: 375px) {
+    box-sizing: border-box;
+    width: 335px;
+    height: 46px;
+    padding: 14px;
+    padding-right: 68px;
+    background-color: transparent;
+    color: #efede8;
+    font-size: 14px;
+    line-height: 18px;
+    border-radius: 12px;
+    border: 1px solid rgba(239, 237, 232, 0.3);
+  }
+
+  @media screen and (min-width: 768px) {
+    width: 236px;
+  }
+
+  @media screen and (min-width: 1440px) {
+    width: 236px;
+    height: 52px;
+    font-size: 16px;
+    line-height: 24px;
+  }
+
+  &:hover,
+  &:focus {
+    outline: none;
+    border-color: #e6533c;
+  }
+
+  &::placeholder {
+    color: #efede8;
+  }
+`;
