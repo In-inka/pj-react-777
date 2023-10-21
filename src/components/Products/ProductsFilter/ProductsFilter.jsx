2edb903e1a0294428d9a5d3e244c10d6509b8059
Ihor Poliakov <piv2003@gmail.com>
2023-10-22 00:16:31 +0200

    Update ProductsFilter.jsx

--- src/components/Products/ProductsFilter/ProductsFilter.jsx
+++ src/components/Products/ProductsFilter/ProductsFilter.jsx
@@ -43,18 +43,85 @@ const categories = [
 ];
 
 export const ProductsFilter = () => {
-  const dispatch = useDispatch(); 
+  const dispatch = useDispatch();
 
-   const capitalizeFirstLeter = string => {
+  const capitalizeFirstLeter = (string) => {
     const newString = string.slice(0, 1).toUpperCase() + string.slice(1);
-     return newString;
+    return newString;
   };
 
+  return (
+    <ProductsFilterList>
+      <li>
+        <ProductsFilterLabel>
+          <ProductsFilterSearch
+            value={search}
+            onChange={onChangeSearch}
+            name="productSearch"
+            type="text"
+            placeholder="Search"
+          />
+          <ProductsBtnClose onClick={delTextInput} type="button">
+            <ProductsSvgClose>
+              <use href={sprite + '#icon-x'}></use>
+            </ProductsSvgClose>
+          </ProductsBtnClose>
+          <ProductsBtnSearch type="button">
+            <ProductsSvgSearch>
+              <use href={sprite + '#icon-search'}></use>
+            </ProductsSvgSearch>
+          </ProductsBtnSearch>
+        </ProductsFilterLabel>
+      </li>
+      <li>
+        <SelectWrapper>
+          <Select
+            styles={customStyles}
+            value={category}
+            onChange={onCategoriesChange}
+            placeholder="Categories"
+            options={categoriesList || []}
+            theme={(theme) => ({
+              ...theme,
 
+              colors: {
+                ...theme.colors,
+                primary_bcwycos: 'rgba(255, 255, 255, 0.10)', //background color when you click on select in the menu
+                primary: 'transparent',
+                neutral_bh: '#EFEDE8', //bird hover
+                neutral_db: 'transparent', //default border
+                neutral_dhb: 'transparent', //default hover border
+                neutral_pc: 'rgba(239, 237, 232, 1)', //placeholder color
+                neutral_n: 'rgba(239, 237, 232, 1)',
+              },
+            })}
+          />
+        </SelectWrapper>
+      </li>
+      <li>
+        <SelectWrapper>
+          <Select
+            styles={customStyles}
+            onChange={onRecomendedChange}
+            value={recommended}
+            theme={(theme) => ({
+              ...theme,
 
-
-  
-return (
-
-)
+              colors: {
+                ...theme.colors,
+                primary_bcwycos: 'rgba(255, 255, 255, 0.10)',
+                primary: 'transparent',
+                neutral_bh: '#EFEDE8',
+                neutral_db: 'transparent',
+                neutral_dhb: 'transparent',
+                neutral_pc: 'rgba(239, 237, 232, 1)',
+                neutral_n: 'rgba(239, 237, 232, 1)',
+              },
+            })}
+            options={optionsRec}
+          />
+        </SelectWrapper>
+      </li>
+    </ProductsFilterList>
+  );
 };
