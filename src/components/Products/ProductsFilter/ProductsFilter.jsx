02a6f12ae2a155bdaab065f7268a105291c6c5d7
Ihor Poliakov <piv2003@gmail.com>
2023-10-21 15:46:03 +0200

    Update ProductsFilter.jsx

---
+++ src/components/Products/ProductsFilter/ProductsFilter.jsx
@@ -0,0 +1,42 @@
+import Select from 'react-select';
+import { useMediaQuery } from 'react-responsive';
+import { useState } from 'react';
+import { useDispatch } from 'react-redux';
+
+import {
+  ProductsFilterLabel,
+  ProductsFilterSearch,
+  ProductsBtnClose,
+  ProductsSvgClose,
+  ProductsBtnSearch,
+  ProductsSvgSearch,
+  SelectWrapper,
+  ProductsFilterList,
+} from './ProductsFilter.styles';
+
+const categories = [
+  'alcoholic drinks',
+  'berries',
+  'cereals',
+  'dairy',
+  'dried fruits',
+  'eggs',
+  'fish',
+  'flour',
+  'fruits',
+  'meat',
+  'mushrooms',
+  'nuts',
+  'oils and fats',
+  'poppy',
+  'sausage',
+  'seeds',
+  'sesame',
+  'soft drinks',
+  'vegetables and herbs',
+];
+
+
+return (
+
+};
