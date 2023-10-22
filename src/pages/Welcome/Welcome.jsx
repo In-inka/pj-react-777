d6c299c2167ba7ce31febdf2c68c887b35e384bb
Bob-UA <120073426+Bob-UA@users.noreply.github.com>
2023-10-22 15:58:41 -0700

    added styles for welcome

--- src/pages/Welcome/Welcome.jsx
+++ src/pages/Welcome/Welcome.jsx
@@ -1,8 +1,57 @@
+import {
+  Container,
+  Title,
+  VideoTutorialContainer,
+  PlayIcon,
+  TextContainerB,
+  VideoTutorialTextA,
+  VideoTutorialTextB,
+  FlexContainer,
+  FlexContainerB,
+  CalorieContainer,
+  RunIcon,
+  TextContainerC,
+  CalorieTextA,
+  CalorieTextB,
+  LogoContainer,
+  BarbellIcon,
+  LogoText,
+  ButtonContainer,
+  ButtonYellow,
+  ButtonBlack,
+} from './Welcome.styled';
+
 const Welcome = () => {
   return (
-    <div className="container">
-      <p>Welcome</p>
-    </div>
+    <Container>
+      <LogoContainer>
+        <BarbellIcon />
+        <LogoText>PowerPulse</LogoText>
+      </LogoContainer>
+      <Title>Transforming your body shape with Power Pulse</Title>
+      <ButtonContainer>
+        <ButtonYellow to="/signup">Sign Up</ButtonYellow>
+        <ButtonBlack to="/signin">Sign In</ButtonBlack>
+      </ButtonContainer>
+      <FlexContainer>
+        <VideoTutorialContainer>
+          <PlayIcon />
+          <TextContainerB>
+            <VideoTutorialTextA>350+</VideoTutorialTextA>
+            <VideoTutorialTextB>Video tutorial</VideoTutorialTextB>
+          </TextContainerB>
+        </VideoTutorialContainer>
+      </FlexContainer>
+      <FlexContainerB>
+        <CalorieContainer>
+          <RunIcon />
+          <TextContainerC>
+            <CalorieTextA>500</CalorieTextA>
+            <CalorieTextB>cal</CalorieTextB>
+          </TextContainerC>
+        </CalorieContainer>
+      </FlexContainerB>
+    </Container>
   );
 };
 
