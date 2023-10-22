93bbb8a75d581dd09035c1d0890cf9937acbfda5
Inna Shkilna <shkinai.1995@gmail.com>
2023-10-22 22:12:05 +0300

    nav

--- src/components/Header/Header.styled.jsx
+++ src/components/Header/Header.styled.jsx
@@ -3,11 +3,4 @@ import styled from 'styled-components';
 export const Container = styled.nav`
   display: flex;
   gap: 15px;
-
-  font-size: 15px;
-  ${
-    '' /*   @media screen and (min-width: 1440px) {
-    position: absolute;
-  } */
-  }
 `;
