d401a4aac935e0e9d77135ee9b23b0cf1f46f23f
vbegunova <118359000+vbegunova@users.noreply.github.com>
2023-10-23 18:14:08 +0300

    Add custom scrollbar

--- src/components/ProductsList/ProductsList.styled.js
+++ src/components/ProductsList/ProductsList.styled.js
@@ -7,4 +7,15 @@ export const ProductsContainer = styled.div`
   display: flex;
   flex-wrap: wrap;
   gap: 32px 16px;
+
+  &::-webkit-scrollbar {
+    width: 8px;
+    background-color: rgba(239, 237, 232, 0.10);
+    border-radius: 12px;
+  }
+
+  &::-webkit-scrollbar-thumb {
+    background: #EF8964;
+    border-radius: 12px;
+  }
 `;
