57249846b1463052c1784e6068258f8676600c4d
In-inka <119360847+In-inka@users.noreply.github.com>
2023-10-23 21:19:39 +0000

    Deploying to gh-pages from @ In-inka/pj-react-777@c07af3096d4a3f5eb0a30a8a4e90cfd104ad4694 🚀

---
+++ assets/Equipment-c6ef7c69.js
@@ -0,0 +1,1 @@
+import{j as t}from"./index-fbf294e1.js";const r=()=>t.jsx("div",{children:"Equipment"});export{r as default};
---
+++ assets/Layout-5714c80b.js
@@ -0,0 +1,4 @@
+import{s as r,j as s,N as e,r as n,O as t}from"./index-fbf294e1.js";const i=r.nav`
+  display: flex;
+  gap: 15px;
+`,l=()=>s.jsx(s.Fragment,{children:s.jsxs(i,{children:["hello",s.jsx(e,{to:"/signup",children:"SignUp"}),s.jsx(e,{to:"/signin",children:"SignIn"}),s.jsx(e,{to:"/profile",children:"Profile"}),s.jsx(e,{to:"/diary",children:"Diary"}),s.jsx(e,{to:"/products",children:"Products"}),s.jsx(e,{to:"/exercises",children:"Exercises"})]})}),o=()=>s.jsxs(s.Fragment,{children:[s.jsx(l,{}),s.jsx(n.Suspense,{fallback:null,children:s.jsx(t,{})})]});export{o as default};
