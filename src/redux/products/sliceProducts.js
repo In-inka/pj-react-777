0c0c7ac59da4be0f8cd8bb2d5aff19200beb21f5
Ihor Poliakov <piv2003@gmail.com>
2023-10-23 18:03:05 +0200

    Update sliceProducts.js

--- src/redux/products/sliceProducts.js
+++ src/redux/products/sliceProducts.js
@@ -1,5 +1,18 @@
 import { createSlice } from '@reduxjs/toolkit';
 import { productListThunk } from './operationsProducts';
 
+function ListFulfilled(state, { payload }) {
+  state.list = payload;
+  state.isLoading = false;
+}
+
+function pending(state) {
+  state.isLoading = true;
+}
+
+function rejected(state) {
+  state.isLoading = false;
+}
+
 export const productsReducer = productSlice.reducer;
 export const filterReducer = productSlice.actions.setFilter;
