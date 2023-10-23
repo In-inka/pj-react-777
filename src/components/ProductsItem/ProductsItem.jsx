2e4ce67b1e33b75045b2b3feeceb18104dd0e09b
vbegunova <118359000+vbegunova@users.noreply.github.com>
2023-10-24 00:56:01 +0300

    Add icons + cut strings

--- src/components/ProductsItem/ProductsItem.jsx
+++ src/components/ProductsItem/ProductsItem.jsx
@@ -10,8 +10,16 @@ import {
   CircleRec,
   RecommendedText,
   AddButton,
-  SpanGrey
+  SpanGrey,
+  IconRunning,
 } from './ProductsItem.styled';
+
+const cutStringLength = (str) => {
+  if (str.length > 26) {
+    return str.substring(0, 26) + '...';
+  }
+  return str;
+};
 
 const ProductsItem = ({ product }) => {
   return (
@@ -20,15 +28,25 @@ const ProductsItem = ({ product }) => {
         <ProductsItemDiet>
           <ProductsItemDietPar>Diet</ProductsItemDietPar>
         </ProductsItemDiet>
-        <div style={{display: 'flex'}}>
+        <div style={{ display: 'flex' }}>
           <ProductsItemRecommendedContainer>
             <CircleRec />
             <RecommendedText>Recommended</RecommendedText>
           </ProductsItemRecommendedContainer>
-          <AddButton>Add -</AddButton>
+          <AddButton>
+            Add
+            <svg width={16} height={16} style={{ stroke: '#E6533C' }}>
+              <use xlinkHref={'/symbol-defs.svg#icon-arrow'} />
+            </svg>
+          </AddButton>
         </div>
       </ProductsItemFunctional>
-      <ProductsItemTitle>{product.title}</ProductsItemTitle>
+      <ProductsItemTitle>
+        <IconRunning width={24} height={24}>
+          <use xlinkHref={'/symbol-defs.svg#icon-running'} />
+        </IconRunning>
+        {cutStringLength(product.title)}
+      </ProductsItemTitle>
       <ProductsItemInfoContainer>
         <ProductsItemInfo>
           <SpanGrey>Calories: </SpanGrey>
