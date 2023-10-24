4d3822d92b3cb548ebb6434b5ea71a085af7aee9
AndriySachyk <asachyk1312@gmail.com>
2023-10-24 17:31:37 +0300

    Create Profile Settings
    Створив розмітку сторінки профайл сетінгс, застилізував

---
+++ src/pages/Profile/Profile-style.js
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+import { ContainerStyled } from "../../Styled/ContainerStyled";
+
+
+
+export const ContainerProfilePage = styled.div`
+    ${ContainerStyled}
+    padding-top: 40px;
+    padding-bottom: 66px;
+`
