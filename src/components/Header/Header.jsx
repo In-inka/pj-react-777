eaf109b83c5c1773845dc0b1756a995500b98db0
Bob-UA <120073426+Bob-UA@users.noreply.github.com>
2023-10-23 21:47:09 -0700

    added styles for header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,16 +1,27 @@
-import { NavLink } from 'react-router-dom';
-import { Container } from './Header.styled';
+import { BarbellIcon, ButtonBlack, ButtonYellow, Container, FlexContainer, LogoContainer, LogoText, LogoutButton, LogoutContainer, LogoutIcon, MenuIcon, SettingsIcon, UserIcon, UserIconContainer } from './Header.styled';
 
 const Header = () => {
   return (
     <>
       <Container>
-        {/* <NavLink to="/signup">SignUp</NavLink>
-        <NavLink to="/signin">SignIn</NavLink>
-        <NavLink to="/profile">Profile</NavLink>
-        <NavLink to="/diary">Diary</NavLink>
-        <NavLink to="/products">Products</NavLink>
-        <NavLink to="/exercises">Exercises</NavLink> */}
+        <FlexContainer>
+          <LogoContainer to="/">
+            <BarbellIcon />
+            <LogoText>PowerPulse</LogoText>
+          </LogoContainer>
+        </FlexContainer>
+        <ButtonBlack to="/diary">Diary</ButtonBlack>
+        <ButtonBlack to="/products">Products</ButtonBlack>
+        <ButtonYellow to="/exercises">Exercises</ButtonYellow>
+        <SettingsIcon />
+        <UserIconContainer to="/profile">
+          <UserIcon />
+        </UserIconContainer>
+        <MenuIcon type="button" />
+        <LogoutContainer>
+          <LogoutButton>Logout</LogoutButton>
+          <LogoutIcon />
+        </LogoutContainer>
       </Container>
     </>
   );
--- src/components/Header/Header.styled.jsx
+++ src/components/Header/Header.styled.jsx
@@ -1,20 +1,169 @@
 import styled from 'styled-components';
 import { ContainerStyled } from '../../Styled/ContainerStyled';
-import { Link } from 'react-router-dom';
+import { Link, NavLink } from 'react-router-dom';
 
 export const Container = styled.nav`
   ${ContainerStyled}
   display: flex;
   position: absolute;
+  z-index: 999;
+  align-items: center;
   top: 0;
   left: 50%;
   transform: translate(-50%, -0%);
   background-color: transparent;
 `;
 
-export const LogoContainer = styled.div`
-`
+export const LogoContainer = styled(NavLink)`
+  display: flex;
+  align-items: center;
+`;
 
-export const Logo = styled(Link)`
-  
-`
+export const FlexContainer = styled.div`
+  display: flex;
+  align-items: center;
+  padding-top: 24px;
+  padding-bottom: 24px;
+  @media screen and (min-width: 768px) {
+    padding-top: 34px;
+    padding-bottom: 33px;
+  }
+`;
+
+export const BarbellIcon = styled.div`
+  width: 36px;
+  height: 13px;
+  background-color: #e6533c;
+  margin-right: 8px;
+`;
+
+export const LogoText = styled.p`
+  font-size: 12px;
+  line-height: 1;
+  font-weight: bold;
+  color: #efede8;
+`;
+
+export const FlexContainerB = styled.div`
+  display: inline-flex;
+  align-items: center;
+  justify-content: end;
+`;
+
+export const SettingsIcon = styled.div`
+  margin-left: auto;
+  width: 24px;
+  height: 24px;
+  border: 3px solid rgba(239, 237, 232, 0.3);
+  border-radius: 50%;
+`;
+
+export const UserIconContainer = styled(NavLink)`
+
+`;
+
+export const UserIcon = styled.div`
+  margin-left: 14px;
+  width: 37px;
+  height: 37px;
+  border: 3px solid #e6533c;
+  border-radius: 50%;
+  @media screen and (min-width: 768px) {
+    margin-left: 16px;
+  }
+`;
+
+export const MenuIcon = styled.button`
+  margin-left: 14px;
+  width: 24px;
+  height: 24px;
+  border: 3px solid #e6533c;
+  @media screen and (min-width: 768px) {
+    margin-left: 16px;
+  }
+  @media screen and (min-width: 1440px) {
+    display: none;
+  }
+`;
+
+
+export const LogoutContainer = styled.div`
+  display: none;
+  @media screen and (min-width: 1440px) {
+    margin-left: 16px;
+    display: block;
+    position: relative;
+  }
+`;
+
+export const LogoutButton = styled.button`
+    border: none;
+    color: #efede8;
+    font-weight: 16px;
+    line-height: 1.5;
+    display: block;
+    background-color: transparent;
+    padding-right: 28px;
+`;
+
+export const LogoutIcon = styled.div`
+  position: absolute;
+  right: 0;
+  top: 50%;
+  transform: translate(-0%, -50%);
+  width: 20px;
+  height: 20px;
+  background-color: #e6533c;
+  pointer-events: none;
+`;
+
+export const ButtonBlack = styled(Link)`
+  display: none;
+  @media screen and (min-width: 1440px) {
+    margin-left: 16px;
+    display: inline-block;
+    font-size: 16px;
+    line-height: 1.5;
+    background-color: inherit;
+    color: #efede8;
+    border-radius: 12px;
+    border: 1px solid rgba(239, 237, 232, 0.3);
+    padding: 10px 27px;
+    transition: border-color 250ms cubic-bezier(0.4, 0, 0.2, 1);
+    &:hover,
+    &:focus {
+      border-color: rgba(230, 83, 60, 1);
+    }
+    &:disabled {
+      border-color: rgba(230, 83, 60, 1);
+    }
+      &:first-of-type {
+        margin-left: 516px;
+      }
+    }
+`;
+
+export const ButtonYellow = styled(Link)`
+  display: none;
+  @media screen and (min-width: 1440px) {
+    margin-left: 16px;
+    display: inline-block;
+    margin-right: 14px;
+    background-color: #e6533c;
+    color: #efede8;
+    border-radius: 12px;
+    border: none;
+    padding: 10px 27px;
+    font-size: 16px;
+    line-height: 1.5;
+    transition: background-color 250ms cubic-bezier(0.4, 0, 0.2, 1);
+    &:hover,
+    &:focus {
+      background-color: #ef8964;
+    }
+    &:disabled {
+      background-color: #e6533c;
+      color: #efede899;
+    }
+  }
+`;
