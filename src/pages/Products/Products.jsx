e5d12cf81239d1dafd2127ad460639841b9d2982
Ihor Poliakov <piv2003@gmail.com>
2023-10-25 19:45:46 +0200

    Update Products.jsx

--- src/pages/Products/Products.jsx
+++ src/pages/Products/Products.jsx
@@ -1,11 +1,12 @@
 import { ProductsList } from '../../components/ProductsList/ProductsList';
 import { Container, ProductsTitle } from './Products.styled';
-
+import { ProductsFilter } from '../../components/ProductsFilter/ProductsFilter';
 const Products = () => {
   return (
     <Container>
       <ProductsTitle>Products</ProductsTitle>
       <ProductsList />
+      <ProductsFilter />
     </Container>
   );
 };
