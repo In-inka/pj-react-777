80234e99587f0488160c7479e704f97adf257081
yuliasiromakha <93142108+yuliasiromakha@users.noreply.github.com>
2023-10-26 20:40:03 +0300

    reusable modal window component

---
+++ src/components/ModalWindow/ModalWindow.jsx
@@ -0,0 +1,78 @@
+import { useState, useEffect, useCallback } from 'react';
+import './ModalWindow.css';
+
+export const ModalWindow = (props) => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const handleClose = () => {
+    setIsOpen(false);
+  };
+
+  const handleEscape = useCallback((e) => {
+    if (e.key === 'Escape') {
+      handleClose();
+    }
+  }, []);
+
+  useEffect(() => {
+    if (props.isOpen) {
+      document.addEventListener('keydown', handleEscape);
+    } else {
+      document.removeEventListener('keydown', handleEscape);
+    }
+
+    return () => {
+      document.removeEventListener('keydown', handleEscape);
+    };
+  }, [props.isOpen, handleEscape]);
+
+  useEffect(() => {
+    setIsOpen(props.isOpen);
+  }, [props.isOpen]);
+
+  const modalStyles = {
+    display: isOpen ? 'flex' : 'none',
+  };
+
+  const contentStyles = {
+    backgroundColor: '#10100F',
+    borderRadius: '8px',
+    boxShadow: '0 2px 4px rgba(0, 0, 0, 0.1)',
+    position: 'relative',
+    padding: '48px 32px',
+    zIndex: 1001,
+    border: '1px solid #EFEDE833',
+    width: `${props.contentWidth}`,
+    height: `${props.contentHeight}`,
+  };
+
+  return (
+    <div className={`modal-backdrop ${isOpen ? 'centered-modal' : ''}`} style={modalStyles}>
+      <div className="modal-content" style={contentStyles}>
+        <span className="close" onClick={handleClose}>
+          &times;
+        </span>
+        {props.children}
+      </div>
+    </div>
+  );
+};
+
+// ПРИКЛАД ВИКОРИСТАННЯ
+// імпортуємо все потрібне
+
+// const [isModalOpen, setIsModalOpen] = useState(false);
+
+// const openModal = () => {
+//   setIsModalOpen(true);
+// };
+
+// <PrimaryButton text={"Open modal window"} onClick={openModal} />
+
+
+// {isModalOpen && (
+//     <ModalWindow isOpen={isModalOpen}>
+//       <div>Modal Content</div>
+//       <SecondaryButton onClick={() => setIsModalOpen(false)} text={"Close modal"} height={50} width={160}/>
+//     </ModalWindow>
+//  )} 
