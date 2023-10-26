49dc13535dfc55dd3be66afe1fc2e5762b042753
IvashkaVIP <ivashka.co@gmail.com>
2023-10-26 12:43:05 +0300

    Tablet done

--- src/pages/SecondPage/Diary.jsx
+++ src/pages/SecondPage/Diary.jsx
@@ -3,12 +3,16 @@ import { DaySwitch } from '../../components/DaySwitch/DaySwitch';
 import { DayProducts } from '../../components/DayProducts/DayProducts';
 import { DayExercises } from '../../components/DayExercises/DayExercises';
 import { DayDashboard } from '../../components/DayDashboard/DayDashboard';
+
 import {
   Container,
   PageTitleText,
   WrapTitle,
   InfoText,
   WrapInfoText,
+  WrapTableBlock,
+  WrapMainBlock,
+  WrapDashBoard,
 } from './Diary.styled';
 
 import sprite from '../../sprite/sprite.svg';
@@ -21,26 +25,29 @@ const Icon = styled.svg`
 const Diary = () => {
   return (
     <Container>
-      
       <WrapTitle>
-        <PageTitleText>Dairy</PageTitleText>
+        <PageTitleText>Diary</PageTitleText>
         <DaySwitch />
       </WrapTitle>
 
-      <DayDashboard />
+      <WrapMainBlock>
+        <WrapDashBoard>
+          <DayDashboard />
+          <WrapInfoText>
+            <IconInfo size={24} />
+            <InfoText>
+              Record all your meals in a calorie diary every day. This will help
+              me be aware of my nutrition and make me responsible for my
+              choices.
+            </InfoText>
+          </WrapInfoText>
+        </WrapDashBoard>
 
-      <WrapInfoText>
-        <IconInfo size={24} />
-        <InfoText>
-          Record all your meals in a calorie diary every day. This will help me
-          be aware of my nutrition and make me responsible for my choices.
-        </InfoText>
-      </WrapInfoText>
-
-      <DayProducts />      
-      <div style={{marginBottom: '40px'}}/>
-      <DayExercises/>
-
+        <WrapTableBlock>
+          <DayProducts />
+          <DayExercises />
+        </WrapTableBlock>
+      </WrapMainBlock>
     </Container>
   );
 };
--- src/pages/SecondPage/Diary.styled.jsx
+++ src/pages/SecondPage/Diary.styled.jsx
@@ -20,38 +20,82 @@ export const Container = styled.div`
 
 export const PageTitleText = styled.h2`
   font-size: 24px;
-  font-style: normal;
-  font-weight: 700;
-  line-height: 1.17;
+  font-weight: bold;
+  line-height: 28px;
   color: ${glodalColor.withe};
 
-  /* @media screen and (min-width: 768px) {
-    margin-bottom: 32px;
+  @media screen and (min-width: 768px) {
     font-size: 32px;
-    line-height: 1.38;
+    line-height: 44px;
+    display: flex;
+    align-items: end;
   }
-
+  /*
   @media screen and (min-width: 1440px) {
     margin-bottom: 0px;
   } */
 `;
 
 export const WrapTitle = styled.div`
-display: flex;
-justify-content: space-between;
-margin-bottom: 40px;
+  display: flex;
+  justify-content: space-between;
+  margin-bottom: 40px;
+
+  @media screen and (min-width: 768px) {
+    height: 64px;
+    margin-bottom: 32px;
+  }
 `;
 
 export const InfoText = styled.p`
+  /* font-family: Roboto, sans-serif; */
   margin-left: 8px;
   font-size: 14px;
   font-style: normal;
   line-height: 18px;
-  color: ${glodalColor.title};  
+  letter-spacing: 0;
+  color: ${glodalColor.title};
+  @media screen and (min-width: 768px) {
+    font-size: 16px;
+    line-height: 24px;
+  }
 `;
 
 export const WrapInfoText = styled.div`
+  max-width: 335px;
   display: flex;
   margin-top: 20px;
   margin-bottom: 40px;
-  `;
+  @media screen and (min-width: 768px) {
+    max-width: 593px;
+    margin-top: 32px;
+    margin-bottom: 64px;
+  }
+  @media screen and (min-width: 1440px) {
+    max-width: 390px;
+  }
+`;
+
+export const WrapDashBoard = styled.div`
+  @media screen and (min-width: 768px) {
+    order: 1;
+  }
+`;
+
+export const WrapTableBlock = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  gap: 40px;
+
+  @media screen and (min-width: 768px) {
+    margin-bottom: 62px;
+    gap: 32px;    
+  }
+`;
+
+export const WrapMainBlock = styled.div`
+  @media screen and (min-width: 768px) {
+    display: flex;
+    flex-wrap: wrap;
+  }
+`;
