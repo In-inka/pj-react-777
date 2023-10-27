838a0bc8c8ff7f6d956b5706d20b0b37c9ecbfca
AndriySachyk <asachyk1312@gmail.com>
2023-10-27 03:55:07 +0300

    Update TitlePage.styled.jsx

--- src/components/TitlePage/TitlePage.styled.jsx
+++ src/components/TitlePage/TitlePage.styled.jsx
@@ -2,7 +2,6 @@ import styled from 'styled-components';
 
 export const Title = styled.h2`
   margin-bottom: 40px;
-  text-transform: capitalize;
   @media screen and (min-width: 768px) {
     margin-bottom: 64px;
   }
