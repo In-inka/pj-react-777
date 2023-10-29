030981a34e153327c2a4d18a598014a7040beec7
Inna Shkilna <shkinai.1995@gmail.com>
2023-10-30 00:39:49 +0200

    avatar

--- src/redux/auth/operations.js
+++ src/redux/auth/operations.js
@@ -4,12 +4,12 @@ import axios from 'axios';
 axios.defaults.baseURL = 'https://power-pulse-rh13.onrender.com/api';
 
 const token = {
-    set(token) {
-        axios.defaults.headers.common.Authorization = `Bearer ${token}`;
-    },
-    unset() {
-        axios.defaults.headers.common.Authorization = ``;
-    },
+  set(token) {
+    axios.defaults.headers.common.Authorization = `Bearer ${token}`;
+  },
+  unset() {
+    axios.defaults.headers.common.Authorization = ``;
+  },
 };
 
 const register = createAsyncThunk(
@@ -25,16 +25,40 @@ const register = createAsyncThunk(
   },
 );
 
-const logIn = createAsyncThunk('/users/logIn', async credentials => {
-    try {
-      const { data } = await axios.post('/users/login', credentials);
-      console.log(data.token);
-        token.set(data.token);
-        return data;
-    } catch (error) {
+const logIn = createAsyncThunk('/users/logIn', async (credentials) => {
+  try {
+    const { data } = await axios.post('/users/login', credentials);
+    console.log(data.token);
+    token.set(data.token);
+    return data;
+  } catch (error) {
     console.log(error.message);
-    }
+  }
 });
+
+const config = {
+  headers: {
+    'Content-Type': 'multipart/form-data',
+  },
+};
+
+const updateUserAvatar = createAsyncThunk(
+  '/users/changeData',
+  async (credentials, thunkAPI) => {
+    try {
+      const { data } = await axios.patch(
+        '/users/changedata',
+        { avatar: credentials },
+        config,
+      );
+      return data;
+    } catch (error) {
+      return thunkAPI.rejectWithValue(error.message);
+    }
+  },
+);
+
+/* то файк відправляється з ключем "avatar"
 
 const updateUserAvatar = createAsyncThunk(
   '/users/changeData',
@@ -46,33 +70,35 @@ const updateUserAvatar = createAsyncThunk(
       return thunkAPI.rejectWithValue(error.message);
     }
   },
+); */
+
+const logOut = createAsyncThunk('/users/logOut', async (credentials) => {
+  try {
+    await axios.post('/users/logout', credentials);
+    token.unset();
+  } catch (error) {
+    console.log(error.message);
+  }
+});
+
+const fetchCurrentUser = createAsyncThunk(
+  'auth/refresh',
+  async (_, thunkAPI) => {
+    const state = thunkAPI.getState();
+    const persistedToken = state.auth.token;
+    if (!persistedToken) {
+      return thunkAPI.rejectWithValue();
+    }
+    token.set(persistedToken);
+    try {
+      const { data } = await axios.get('/users');
+      console.log(data);
+      return data;
+    } catch (error) {
+      console.log(error.message);
+    }
+  },
 );
-
-const logOut = createAsyncThunk('/users/logOut', async credentials => {
-  try {
-      await axios.post('/users/logout', credentials);
-      token.unset();
-  } catch (error) {
-    console.log(error.message);
-  }
-});
-
-
-const fetchCurrentUser = createAsyncThunk('auth/refresh', async (_, thunkAPI) => {
-  const state = thunkAPI.getState();
-  const persistedToken = state.auth.token;
-  if (!persistedToken) {
-    return thunkAPI.rejectWithValue();
-  }
-  token.set(persistedToken);
-  try {
-    const { data } = await axios.get('/users');
-    console.log(data);
-    return data;
-  } catch (error) {
-    console.log(error.message);
-  }
-});
 
 const updateUserMetricsData = createAsyncThunk(
   '/userMetric',
