5cb097e8def8c1871d3f82b86bca8d2f9a4a8cb8
Ihor Poliakov <piv2003@gmail.com>
2023-10-30 11:02:43 +0200

    Update ProductsList.jsx

--- src/components/ProductsList/ProductsList.jsx
+++ src/components/ProductsList/ProductsList.jsx
@@ -1,13 +1,17 @@
 import { ProductsContainer } from './ProductsList.styled';
 import { ProductsItem } from '../ProductsItem/ProductsItem';
+import { SearchNotResult } from '../SearchNotResult/SearchNotResult';
 
 const ProductsList = ({ products }) => {
   return (
     <ProductsContainer>
-      {products?.length > 0 ?
+      {products?.length > 0 ? (
         products.map((product) => {
           return <ProductsItem key={product._id} product={product} />;
-        }) : <h1>No products</h1>}
+        })
+      ) : (
+        <SearchNotResult />
+      )}
     </ProductsContainer>
   );
 };
