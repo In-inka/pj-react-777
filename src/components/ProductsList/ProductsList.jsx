bd48f804f169eab904d405c2c40a6d3881a6e4c0
Serhii Ozeran <ozeransm@gmail.com>
2023-11-02 08:38:36 +0200

    update

--- src/components/ProductsList/ProductsList.jsx
+++ src/components/ProductsList/ProductsList.jsx
@@ -2,7 +2,7 @@ import { ProductsContainer } from './ProductsList.styled';
 import { ProductsItem } from '../ProductsItem/ProductsItem';
 import { SearchNotResult } from '../SearchNotResult/SearchNotResult';
 import { useInView } from 'react-intersection-observer';
-import { useEffect, useRef, useRef, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import { nanoid } from 'nanoid';
 import { useDispatch, useSelector } from 'react-redux';
 import {
@@ -11,13 +11,13 @@ import {
   selectPage,
   selectTotal,
   selectModalOpen,
-  selectPage,
   selectSuccessModal,
 
 } from '../../redux/products/selectorsProducts';
 import AddProductForm from '../AddProductForm/AddProductForm';
 import { modalReducer, successModalReducer } from '../../redux/products/sliceProducts';
 import { AddProductSuccess } from '../AddProductSuccess/AddProductSuccess';
+import diarySelectors from '../../redux/diary/diarySelectors';
 
 const ProductsList = ({ products, fetching }) => {
   const pageStr = Number(useSelector(selectPage));
